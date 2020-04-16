ea38a53e84e9ee2d87fe5ded9ee8e807104cc14b
Dave Pernitz <pernitz@uwalumni.com>
2020-04-15 20:15:58 -0600

    Add room class and test

--- src/Room.js
+++ src/Room.js
@@ -5,8 +5,29 @@ class Room {
     this.bidet = roomData.bidet;
     this.bedSize = roomData.bedSize;
     this.numBeds = roomData.numBeds;
-    this.costPerNoght = roomData.costPerNight
+    this.costPerNight = roomData.costPerNight
+    this.bookings = []
   }
+
+  checkIfBooked(date) {
+    let isBooked = false;
+    this.bookings.forEach(booking => {
+      if (booking.date === date) {
+        isBooked = true;
+      }
+    })
+    return isBooked;
+  }
+
+  book(booking) {
+    this.bookings.push(booking)
+  }
+
+  unBook(id) {
+    let index = this.bookings.findIndex(booking => booking.id === id)
+    this.bookings.splice(index, 1)
+  }
+  
 }
 
 

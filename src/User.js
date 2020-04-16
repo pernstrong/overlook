ec7848601f6ac61fa577938d704280970196ee42
Dave Pernitz <pernitz@uwalumni.com>
2020-04-15 20:15:27 -0600

    Add user class and test

--- src/User.js
+++ src/User.js
@@ -2,6 +2,8 @@ class User {
   constructor(userData) {
     this.id = userData.id;
     this.name = userData.name;
+    this.pastBookings = [];
+    this.upcomingBookings = [];
   }
 }
 
--- test/user-test.js
+++ test/user-test.js
@@ -1,66 +1,69 @@
 import User from '../src/User';
-const chai = require('chai')
-  , spies = require('chai-spies');
+// const chai = require('chai')
+const spies = require('chai-spies');
 chai.use(spies)
 
 import chai from 'chai';
 const expect = chai.expect;
 
-describe('See if the tests are running', function() {
+describe('User', function() {
   
-    let user1;
+  let user1;
 
   this.beforeEach(function () {
    
-    user1 = new User(1, 'Billy Beans')
-
+    user1 = new User({id: 1, name: 'Billy Beans'})
   })
   
-  
-    it('should have be a function', function() {
-
+  it('should have be a function', function() {
     expect(User).to.be.a('function');
   });
 
-  it.skip('should be an instance of User', function() {
-        
+  it('should be an instance of User', function() {  
     expect(user1).to.be.an.instanceof(User);
   });
 
-  it.skip('should have an id', function() {
-        
+  it('should have an id', function() { 
     expect(user1.id).to.equal(1);
   });
 
-  it.skip('should have a name', function() {
+  it('should have a name', function() {
         
     expect(user1.name).to.equal('Billy Beans');
   });
 
-  it.skip('should hold its past room bookings', function() {
+  it('should hold its past room bookings', function() {
+    const booking1 = {id: 'abc123', userID: 9, date: '2020/02/04', roomNumber: 15, roomServiceCharges: []}
+    const booking2 = {id: 'five', userID: 2, date: '2020/02/03', roomNumber: 13, roomServiceCharges: []}
 
-    expect(user1.pastBookings).to.deep.equal()
+    user1.pastBookings.push(booking1)
+    user1.pastBookings.push(booking2)
+
+    expect(user1.pastBookings.length).to.equal(2)
+    expect(user1.pastBookings[1].roomNumber).to.equal(13)
   });
   
-  it.skip('should hold its upcoming room bookings', function() {
+  it('should hold its upcoming room bookings', function() {
 
-    expect(user1.upcomingBookings).to.deep.equal()
+    const booking1 = {id: 'ten', userID: 9, date: '2020/05/04', roomNumber: 12, roomServiceCharges: []}
+    const booking2 = {id: 'eleven', userID: 2, date: '2020/06/03', roomNumber: 10, roomServiceCharges: []}
+
+    user1.upcomingBookings.push(booking1)
+    user1.upcomingBookings.push(booking2)
+    
+    expect(user1.upcomingBookings.length).to.equal(2)
+    expect(user1.upcomingBookings[0].id).to.equal('ten')
   });
+ 
+  // it.skip('should calculate how much the user has spent on rooms', function() {
 
-  it.skip('should calculate how much the user has spent on rooms', function() {
+  //   expect(user1.).to.equal()
+  // });
 
-    expect(user1.).to.equal()
-  });
+  // it.skip('should calculate how much the user has spent on rooms', function() {
 
-  it.skip('should calculate how much the user has spent on rooms', function() {
-
-    expect(user1.pastBookings).to.deep.equal()
-  });
-
-
-
-
-
+  //   expect(user1.pastBookings).to.deep.equal()
+  // });
 
 
 
